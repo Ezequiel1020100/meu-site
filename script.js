e128aa10ef9e8848f080287de8848b3513777fd5
ezequiel brito <ezequielbritojesus@gmail.com>
2025-01-20 11:40:47 -0300

    fim do front

--- script.js
+++ script.js
@@ -1,7 +1,67 @@
 const menu = document.getElementById("menu")
 const btn1 = document.getElementById("btn-open")
 const btn2 = document.getElementById("btn-close")
-const links = document.getElementById("links")
+const section1 = document.getElementById("bnt-section-1")
+const  section2 = document.getElementById("bnt-section-2")
+const  section3  = document.getElementById("bnt-section-3")
+const login = document.getElementById("login")
+const home = document.getElementById("home")
+const duvidas   = document.getElementById("duvidas")
+const compras = document.getElementById("compras")
+const mais = document.getElementById("mais")
+
+
+
+
+
+section1.addEventListener("click", () =>
+    informar())
+    function informar(){
+    alert("Em desenvolvimento")
+        }
+section2.addEventListener("click", () =>
+ informar())
+ function informar(){
+ alert(" Em desenvolvimento")
+  }
+section3.addEventListener("click", () =>
+ informar())
+ function informar(){
+ alert("Em desenvolvimento")
+ }
+                
+ home.addEventListener("click", () =>
+    informar())
+    function informar(){
+    alert("Em desenvolvimento")
+    }
+login.addEventListener("click", () =>
+        informar())
+        function informar(){
+        alert(" Em desenvolvimento")
+        }
+duvidas.addEventListener("click", () =>
+informar())
+ function informar(){
+alert("Em desenvolvimento")
+            }
+compras.addEventListener("click", () =>
+informar())
+ function informar(){
+alert("Em desenvolvimento")
+        }
+
+mais.addEventListener("click", () =>
+ informar())
+ function informar(){
+alert("Em desenvolvimento")
+    }
+
+
+
+
+
+
 btn1.addEventListener("click", () =>
 iniciar())
 function iniciar(){
@@ -18,3 +78,4 @@ btn2.addEventListener("click", () =>
     btn2.style.display = "none"
     links.style.display = "none"
     }
+
